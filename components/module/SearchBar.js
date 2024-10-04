946e3faeba6103997efd159efd704e91cff318eb
Ali FateFul <ali.fateful21@gmail.com>
2024-10-04 20:56:12 +0330

    SearchBar Developed

--- components/module/SearchBar.js
+++ components/module/SearchBar.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
+import { useRouter } from "next/router";
 function SearchBar() {
+	const router = useRouter();
+	const [min, setMin] = useState("");
+	const [max, setMax] = useState("");
+
+	const searchHandler = () => {
+		if (min && max) {
+			router.push(`/filter/${min}/${max}`);
+		} else {
+			alert("Please Enter a valid Min and Max Price");
+		}
+	};
 	return (
 		<div className={styles.container}>
 			<div>
-				<input placeholder='Enter min-price' />
-				<input placeholder='Enter max-price' />
+				<input
+					placeholder='Enter min-price'
+					value={min}
+					onChange={(e) => setMin(e.target.value)}
+				/>
+				<input
+					placeholder='Enter max-price'
+					value={max}
+					onChange={(e) => setMax(e.target.value)}
+				/>
 			</div>
-			<button>Search</button>
+			<button onClick={searchHandler}>Search</button>
 		</div>
 	);
 }
