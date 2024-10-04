21b1dd5af67124b74df3d8f05e858c1b24b8cefa
Ali FateFul <ali.fateful21@gmail.com>
2024-10-04 20:20:49 +0330

    search bar created

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,4 +1,5 @@
 import Categories from "../../components/module/Categories";
+import SearchBar from "../../components/module/SearchBar";
 import CarsPage from "../../components/templates/CarsPage";
 
 import carsData from "../../data/carsData";
@@ -6,6 +7,7 @@ import carsData from "../../data/carsData";
 function cars() {
 	return (
 		<div>
+			<SearchBar />
 			<Categories />
 			<CarsPage data={carsData} />
 		</div>
